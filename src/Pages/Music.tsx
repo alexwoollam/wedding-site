9bddaf579cb9ade5337c627c572d4f4e1289b905
Alex Woollam <alexjhwoollam@gmail.com>
2021-03-11 21:03:24 +0000

    started spotify intergration, now serchs tracks.. noice.

--- src/Pages/Music.tsx
+++ src/Pages/Music.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Heading from '../Components/Text/Heading';
-import SpotifyToken from '../Controllers/Music/SpotifyToken';
-//import Spotify from 'spotify-web-api-js';
+import SearchTrack from '../Components/Music/SearchTrack';
 
 const text = ({
   intro: "Croud-sources playlist!",
@@ -9,32 +8,11 @@ const text = ({
 
 function Music() {
 
-    const [theToken, setTheToken] = useState();
-
-    useEffect(() => {
-        if( ! theToken ){
-            const CLIENT_ID:string = process.env.REACT_APP_SPOTIFYCLI!;
-            const CLIENT_SEC:string = process.env.REACT_APP_SPOTIFYSEC!;
-
-            var token = btoa( CLIENT_ID + ':' + CLIENT_SEC );
-
-            const recived_token = fetch("https://accounts.spotify.com/api/token", {
-            body: "grant_type=client_credentials",
-            headers: {
-                'Authorization': 'Basic ' + token,
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-            },
-            method: "POST"
-            })
-            .then(response => response.json())
-            .then( data => setTheToken( data.access_token ) );
-        }       
-    })
-
   return (
     <div className="App container mx-auto">
       <div className="Intro">
         <Heading text={text.intro}/>
+        <SearchTrack/>
       </div>
     </div>
   );
