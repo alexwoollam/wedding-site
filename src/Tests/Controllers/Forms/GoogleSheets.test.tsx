19b0a85cdbfd6799fac332040e34c5d8cc9f4263
Alex Woollam <alex.woollam@proagrica.com>
2021-03-12 15:29:58 +0000

    mooooore unit testing..

--- src/Tests/Controllers/Forms/GoogleSheets.test.tsx
+++ src/Tests/Controllers/Forms/GoogleSheets.test.tsx
@@ -4,9 +4,17 @@ import { render, screen } from '@testing-library/react';
 import GoogleSheets from '../../../Controllers/Forms/GoogleSheets';
 
 describe('GoogleSheets controller', () => {
-    test('expect google sheets function to return false with incompleate information', () => {
+    
+    test('expect google sheets function to early return false with incompleate information', () => {
         const data = { Name: '', Email: '' }
-        const test = GoogleSheets( data );
+        const test = GoogleSheets( data, '1234', '1234' );
         expect(test).toBe( false );
     });
+
+    test('expect google sheets function to return true with compleate information', () => {
+        const data = { Name: 'Alex Test', Email: 'test@test.com' }
+        const test = GoogleSheets( data, '1234', '1234' );
+        expect(test).toBe( true );
+    });
+
 });
