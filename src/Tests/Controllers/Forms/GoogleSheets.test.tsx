e7dc9f86460e19b53fb25fb33019cc06626b651a
Alex Woollam <alexjhwoollam@gmail.com>
2021-03-14 17:06:21 +0000

    updated Google sheets test

--- src/Tests/Controllers/Forms/GoogleSheets.test.tsx
+++ src/Tests/Controllers/Forms/GoogleSheets.test.tsx
@@ -17,4 +17,22 @@ describe('GoogleSheets controller', () => {
         expect(test).toBe( true );
     });
 
+    test('expect google sheets function to return false with null creds', () => {
+        process.env.REACT_APP_G_USR = 'null';
+        process.env.REACT_APP_G_KEY = 'null';
+
+        const data = { Name: 'Alex Test', Email: 'test@test.com' }
+        const test = GoogleSheets( data, '1234', '1234' );
+        expect(test).toBe( false );
+    });
+
+    test('expect google sheets function to return true with working creds', () => {
+        process.env.REACT_APP_G_USR = process.env.REACT_APP_G_USR;
+        process.env.REACT_APP_G_KEY = process.env.REACT_APP_G_KEY;
+
+        const data = { Name: 'Alex Test', Email: 'test@test.com' }
+        const test = GoogleSheets( data, '1234', '1234' );
+        expect(test).toBe( false );
+    });
+
 });
