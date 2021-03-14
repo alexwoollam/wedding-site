2837c5928a18767e56a725d3098c7e408fcdb150
Alex Woollam <alexjhwoollam@gmail.com>
2021-03-14 15:42:09 +0000

    added tests for tracks comp

--- src/Tests/Components/Music/Track.test.tsx
+++ src/Tests/Components/Music/Track.test.tsx
@@ -5,41 +5,60 @@ import renderer from 'react-test-renderer';
 import { render, screen } from '@testing-library/react';
 import Track from '../../../Components/Music/Track';
 
-describe('<TextBlock/> component', () => {
+describe('<Track/> component', () => {
 
     let MockTracks = new Object();
-    MockTracks = {
-        'items': {
-            'album': {
-                'images': [
-                    {'url': "https://i.scdn.co/image/ab67616d00001e02d99d0823b831a78b50379100"},
-                    {'url': "https://i.scdn.co/image/ab67616d00001e02d99d0823b831a78b50379100"}
-                ]
-            },
-            'artists': [ 
-                'name': "Blur" 
-            ],
-            'id': "1FTSo4v6BOZH9QxKc3MbVM",
-            'name': "Song 2 - 2012 Remaster",
-            'preview_url': "https://p.scdn.co/mp3-preview/183c0855e94b58dcb267e2b0721d4a3c99260acf?cid=1686551f71a7417da335f1cb88526648",
-        }
-    }
+    MockTracks = [
+            {'some string':'string'},
+            [
+                {
+                    'album': {
+                        'images': [
+                            {'url': "https://i.scdn.co/image/ab67616d00001e02d99d0823b831a78b50379100"},
+                            {'url': "https://i.scdn.co/image/ab67616d00001e02d99d0823b831a78b50379100"},
+                            {'url': "https://i.scdn.co/image/ab67616d00001e02d99d0823b831a78b50379100"},
+                        ]
+                    },
+                    'artists': [{
+                        'name': 'Blur' 
+                    }],
+                    'id': "1FTSo4v6BOZH9QxKc3MbVM",
+                    'name': "Song 2",
+                    'preview_url': "https://p.scdn.co/mp3-preview/183c0855e94b58dcb267e2b0721d4a3c99260acf?cid=1686551f71a7417da335f1cb88526648",
+                },
+                {
+                    'album': {
+                        'images': [
+                            {'url': "https://i.scdn.co/image/ab67616d00001e02d99d0823b831a78b50379100"},
+                            {'url': "https://i.scdn.co/image/ab67616d00001e02d99d0823b831a78b50379100"},
+                            {'url': "https://i.scdn.co/image/ab67616d00001e02d99d0823b831a78b50379100"},
+                        ]
+                    },
+                    'artists': [{
+                        'name': 'Nirvana' 
+                    }],
+                    'id': "1FTSo4v6BOZH9QxKc3MbVM",
+                    'name': "Bleach",
+                    'preview_url': "https://p.scdn.co/mp3-preview/183c0855e94b58dcb267e2b0721d4a3c99260acf?cid=1686551f71a7417da335f1cb88526648",
+                },
+            ]
+        ];
 
     test('Track title should be Song 2', () => {
         render(<Track data={ MockTracks } />);
-        const linkElement = screen.getByText('Song 2 - 2012 Remaster');
+        const linkElement = screen.getByText('Song 2');
         expect(linkElement).toBeInTheDocument();
     });
 
     test('Artist should be blur', () => {
         render(<Track data={ MockTracks }/>);
-        const linkElement = screen.queryByTestId('Blur');
-        expect(linkElement).not.toBeInTheDocument();
+        const linkElement = screen.getByText('Blur');
+        expect(linkElement).toBeInTheDocument();
     });
 
     it('renders correctly', () => {
         const tree = renderer
-        .create(<Track  />).toJSON();
+        .create(<Track data={ MockTracks } />).toJSON();
         expect(tree).toMatchSnapshot();
     });
 });
