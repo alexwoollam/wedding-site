d6a977b5cc2d647b43abb1b69c1a0c2dfb904372
Alex Woollam <alexjhwoollam@gmail.com>
2021-03-14 17:19:22 +0000

    updated Google sheets test

--- src/Tests/Controllers/Forms/GoogleSheets.test.tsx
+++ src/Tests/Controllers/Forms/GoogleSheets.test.tsx
@@ -2,6 +2,8 @@ import '@testing-library/jest-dom';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import GoogleSheets from '../../../Controllers/Forms/GoogleSheets';
+import dotenv from 'dotenv';
+
 
 describe('GoogleSheets controller', () => {
     
@@ -17,18 +19,15 @@ describe('GoogleSheets controller', () => {
         expect(test).toBe( true );
     });
 
+    test('expect google sheets function to return true with working creds', () => {
+        const data = { Name: 'Alex Test', Email: 'test@test.com' }
+        const test = GoogleSheets( data, '179SUOlAtTT5EPP3Zx1g4yGivGFIZzu_ScNiLgGJWn6o', '334621642' );
+        expect(test).toBe( true );
+    });
+
     test('expect google sheets function to return false with null creds', () => {
         process.env.REACT_APP_G_USR = 'null';
         process.env.REACT_APP_G_KEY = 'null';
-
-        const data = { Name: 'Alex Test', Email: 'test@test.com' }
-        const test = GoogleSheets( data, '1234', '1234' );
-        expect(test).toBe( false );
-    });
-
-    test('expect google sheets function to return true with working creds', () => {
-        process.env.REACT_APP_G_USR = process.env.REACT_APP_G_USR;
-        process.env.REACT_APP_G_KEY = process.env.REACT_APP_G_KEY;
 
         const data = { Name: 'Alex Test', Email: 'test@test.com' }
         const test = GoogleSheets( data, '1234', '1234' );
