2943b2315fa51a4c7f420f964f0bfd667aed432e
Alex Woollam <alexjhwoollam@gmail.com>
2021-03-14 16:29:20 +0000

    updated App test

---
+++ src/Tests/App.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@testing-library/jest-dom';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+jest.mock("react-dom", () => ({ render: jest.fn() }));
+
+it('App renders without crashing', () => {
+    const div = document.createElement("div");
+    div.id = "root";
+    document.body.appendChild(div);
+    require("../index.tsx");
+    expect(ReactDOM.render).toHaveBeenCalledWith(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>, div);
+});
