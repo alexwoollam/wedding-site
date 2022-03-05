58d154e592366ebe50b0713ee0629f2a4cd2e378
Alexander Woollam <alexjhwoollam@gmail.com>
2022-03-05 15:35:20 +0000

    reset

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,38 @@ import { store } from './store';
 import { Provider } from 'react-redux'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Protect from 'react-app-protect'
 import 'react-app-protect/dist/index.css'
+
+const PASSWORD:string = process.env.REACT_APP_PASSWORD!;
 
 ReactDOM.render(
   <React.StrictMode>
-
+      <Protect
+          styles={{
+              input: {
+                  color: '#67805a',
+                  width: '100%',
+              },
+              header: {
+                  fontSize: '15px',
+                  background: '#67805a',
+              },
+              button : {
+                  border: '2px solid #67805a',
+                  color: '#67805a',
+                  backgroundColor: 'rgba(0,0,0,0)',
+              }
+          }}
+          boxTitle="Please use the password printed on your invite"
+          inputPlaceholder={'Password (All Caps!)'}
+          blur={true}
+          sha512={PASSWORD}
+      >
           <Provider store={ store }>
               <App />
           </Provider>
+      </Protect>
   </React.StrictMode>,
   document.getElementById('root')
 );
